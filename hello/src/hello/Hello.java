f03e6a932c60e63be1eb0b10aaba5e5e341b965f
취업반PM <PM@itbank07-PC>
2015-05-08 21:35:51 +0900

    구구단이라고 제목을 주었음..

--- hello/src/hello/Hello.java
+++ hello/src/hello/Hello.java
@@ -2,11 +2,10 @@ package hello;
 
 class Hello {
 	public static void main(String[] args) {
-		for (int i = 2; i < 8; i += 4) {
+		for (int i = 2; i < 7; i += 4) {
 			for (int j = 1; j < 10; j++) {
 				for (int k = i; k < i + 4; k++) {
-					System.out.print(k + " X " + j + " = " + k * j);
-					System.out.print("\t");
+					System.out.print(k + " X " + j + " = " + k * j+"\t");
 				}
 				System.out.println();
 			}
