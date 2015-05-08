f09c1a3d87a904035a28909dd9cbf16607cda65e
취업반PM <PM@itbank07-PC>
2015-05-08 21:40:51 +0900

    별그리기.....

--- hello/src/star111.java
+++ hello/src/star111.java
@@ -1,12 +1,10 @@
-
 public class star111 {
-
 	public static void main(String[] args) {
-		for(int i=1; i<10; i++){
-			for(int j=10; j>i; j--){
+		for (int i = 1; i < 10; i++) {
+			for (int j = 10; j > i; j--) {
 				System.out.print("* ");
-			}System.out.println();
+			}
+			System.out.println();
 		}
 	}
-
 }
